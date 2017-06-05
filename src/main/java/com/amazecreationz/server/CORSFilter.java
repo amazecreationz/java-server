85c68a8161aae7c223877a6bc95d3b02c6e5efb4
Anand Mohan <moghan.anand@gmail.com>
2017-06-05 23:39:58 +0530

    CORS Filter added.

---
+++ src/main/java/com/amazecreationz/server/CORSFilter.java
@@ -0,0 +1,42 @@
+package com.amazecreationz.server;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(description = "For adding cors headers", asyncSupported = true, urlPatterns = { "/*" })
+public class CORSFilter implements Filter {
+
+    public CORSFilter() {
+    }
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+ 
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+ 
+        HttpServletResponse resp = (HttpServletResponse) response;
+ 
+        if (httpRequest.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+        
+        chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
